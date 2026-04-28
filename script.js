499fbcb8d994b71790d73146edd1d487f2883071
cap1kep-prog <cap1kep@gmail.com>
2026-04-28 15:30:37 +0700

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-async function nopBai() {
-  try {
-    let ten = prompt("Nhập tên của em:");
+function nopBai() {
+  const noiDung = "Bài của học sinh";
+  const data = btoa(unescape(encodeURIComponent(noiDung)));
 
-    const response = await fetch("https://script.google.com/macros/s/AKfycbznmu6bq1h1gU65vjNSBCCcG8vr5WnU46R53_030Sptysfk6qOD0G7k6vLxhSZcoZbKcQ/exec", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        name: ten + "_" + Date.now() + ".txt",
-        file:const data = btoa(unescape(encodeURIComponent(noiDung)));
-      })
-    });
-
-    const result = await response.json();
-
-    if (result.status === "success") {
-      alert("✅ Nộp bài thành công!");
-    } else {
-      alert("❌ Lỗi!");
-    }
-
-  } catch (err) {
-    alert("❌ Lỗi: " + err);
-  }
+  fetch("LINK_APPS_SCRIPT", {
+    method: "POST",
+    body: JSON.stringify({ data: data })
+  });
 }
